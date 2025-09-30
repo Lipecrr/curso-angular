eb55f9c4d47f629ffb8375efe2e31d2d774578f0
Felipe Fernando Correa <lipeaba13@gmail.com>
2025-09-29 21:57:44 -0300

    Aula 29/09/2025

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavbarComponent, NavbarMenu } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [NavbarComponent],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  menus: NavbarMenu[];
+
+  constructor() {
+    this.menus = [
+      { link: "calculadora", titulo: "Calculadora" },
+      { link: "lista-pessoas", titulo: "Lista de pessoas" },
+      { link: "calculadora-retangulo", titulo: "Calculadora retangulo" },
+      { link: "calculadora-media", titulo: "Calculadora média" },
+      { link: "calculadora-temperatura", titulo: "Calculadora temperatura" },
+      { link: "lista-tarefas", titulo: "Lista de tarefas" },
+      { link: "lista-alunos", titulo: "Lista de alunos" },
+      { link: "lista-turmas", titulo: "Lista de turmas" },
+      { link: "lista-materias", titulo: "Lista de materias" },
+      { link: "lista-professores", titulo: "Lista de professores" },
+    ]
+  }
 }
