4ddfecff1546bc5b678d361681439f39ba21c43b
Felipe Fernando Correa <lipeaba13@gmail.com>
2025-10-01 22:02:07 -0300

    Aula 01/10/2025

---
+++ biblioteca-ng/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CategoriaList } from './pages/list/list';
+
+export const routes: Routes = [
+    { path: "categorias", component: CategoriaList }
+];
